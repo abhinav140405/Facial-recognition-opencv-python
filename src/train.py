fbd35c3f8d6be36ad8b668926b5d86a2d550588c
abhinav <rajamainaabhinav@gmail.com>
2025-10-22 17:46:56 +0530

    train.py

---
+++ src/train.py
@@ -0,0 +1,29 @@
+import argparse
+import cv2
+import os
+import pickle
+import numpy as np # Moved import outside of if block
+# from utils import prepare_training_data, ensure_dir # Removed import
+def main(data_dir, model_path):
+    ensure_dir(os.path.dirname(model_path) or '.')
+    faces, labels, label_map = prepare_training_data(data_dir)
+    if len(faces) == 0:
+        print('No faces found in dataset. Please capture images first.')
+        return
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.train(faces, np.array(labels))
+    recognizer.write(model_path)
+    # Save label map
+    with open(model_path + '.labels.pkl', 'wb') as f:
+        pickle.dump(label_map, f)
+    print(f'Trained model saved to {model_path}')
+# if __name__ == '__main__': # Removed argparse
+#     # import numpy as np # Removed import from here
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--data-dir', default='dataset', help='Dataset directory')
+#     parser.add_argument('--model', default='models/lbph_model.xml', help='Output model file')
+#     args = parser.parse_args()
+#     main(args.data_dir, args.model)
+
+# Direct call to main with default values
+main(data_dir='dataset', model_path='models/lbph_model.xml')
